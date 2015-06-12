5aa38cc7d6d032a01d139d46d73eedbefdec66e8
congye <congye@hengtiansoft.com>
2015-06-12 17:45:05 +0800

    2utf-8

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //1.Factory Method
-        //??????
+        //普通工厂模式
         /*new SenderFactory().produce("sms").send();
         new SenderFactory().produce("mail").send();
         Sender isnullSender = new SenderFactory().produce("twen");
@@ -15,16 +15,16 @@ public class Main {
             System.out.println("the Sender is null...");
         }*/
 
-        //????????
+        //多个工厂方法模式
         /*new SenderFactory2().produceSms().send();
         new SenderFactory2().produceMail().send();*/
 
-        //????????
+        //静态工厂方法模式
         //SenderFactory3.produceSmsSender().send();
         //SenderFactory3.pruduceMailSender().send();
 
         //2.Abstract Factory
-        //??????
+        //抽象工厂模式
         new SenderMailFactory().produce().send();
         new SenderSmsFactory().produce().send();
 
